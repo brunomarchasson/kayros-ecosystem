4d6c56dc804ac67afd672e3ecdc85c7fd3e0530a
bruno marchasson <bruno.marchasson@gmail.com>
2022-09-06 16:15:39 +0200

    localisation

--- web/index.js
+++ web/index.js
@@ -1,33 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Switchn, Route, useParams } from "react-router-dom";
-
+import { I18nextProvider } from 'react-i18next';
+import {
+  BrowserRouter,
+} from 'react-router-dom';
+// eslint-disable-next-line no-unused-vars
+import i18n from './i18n';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const Api = () => {
-  const [apiId, setApiId] = useState('')
+function Api() {
+  const [apiId, setApiId] = useState('');
 
-  useEffect(()=> {
-    const parsedData = window.location.pathname.split("/");
-    let api = parsedData[1];
-    setApiId(api)
-  }, [window.location.pathname])
-
-  console.log('eee', apiId)
-
+  useEffect(() => {
+    const parsedData = window.location.pathname.split('/');
+    const api = parsedData[1];
+    setApiId(api);
+  }, [window.location.pathname]);
   return (
-    <BrowserRouter basename={'/'+apiId} >
-      <App apiId={apiId}/>
+    <BrowserRouter basename={ `/${apiId}` }>
+      <I18nextProvider i18n={ i18n }>
+        <App apiId={ apiId } />
+      </I18nextProvider>
     </BrowserRouter>
   );
-};
+}
 root.render(
   <React.StrictMode>
-  <Api></Api>
-  </React.StrictMode>
+    <Api />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
