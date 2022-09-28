af96590ae081a0230db1b5f85793bf6b18e902e9
bruno marchasson <bruno.marchasson@gmail.com>
2022-09-28 17:48:59 +0200

    jest

--- api/test/e2e/sqlConnect.test.js
+++ api/test/e2e/sqlConnect.test.js
@@ -1,16 +1,36 @@
-import db from "../../core/db";
+// import { DBhooks } from "./helper/db";
+const sql =require("mssql");
+const { sqlConfig } = require("./helper/db");
 
-describe("Database", () => {
-  beforeAll(() => {});
-  it("should query with parameters", async () => {
-    // Arrange
-    const query = "SELECT (:p + 5) as res";
-    const params = { p: 73 };
-
-    const expectedResult = 78;
-
-    const result = await db.raw(query, params);
-
-    expect(result[0]?.res).toBe(expectedResult);
-  });
+let transaction;
+let request;
+beforeAll(async () => {
+  // Init the server
+  const pool = await sql.connect(sqlConfig).catch(console.error);
+  transaction = new sql.Transaction(pool);
+  await transaction.begin();
+  request = new sql.Request(transaction);
 });
+
+afterAll(async () => {
+  jest.resetAllMocks();
+  jest.resetModules();
+  await transaction.rollback();
+});
+describe("Database", () => {
+  it("should query with parameters", async () => {
+    // transaction.begin(async () => {
+
+      // Arrange
+      // db.input('p', sql.Int, 73).query('SELECT (@p + 5) as res')
+      // expect(true).toBe(true)
+      const expectedResult = 78;
+      // const request = new sql.Request(transaction)
+      const result = await request
+        .input("p", sql.Int, 73)
+        .query("SELECT (@p + 5) as res");
+      expect(result.recordset[0]?.res).toBe(expectedResult);
+      //  transaction.rollback();
+    });
+  });
+// });
