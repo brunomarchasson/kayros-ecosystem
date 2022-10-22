f00488a85cf7c8f0ab33d13a8e6e8786b3650f00
brunomarchasson <brunomarchasson@users.noreply.github.com>
2022-10-22 11:47:20 +0200

    Update apiConfig.js

--- api-gateway/apiConfig.js
+++ api-gateway/apiConfig.js
@@ -4,6 +4,8 @@ module.exports = {
 
 
   local: 'http://localhost:8000',
-  dev: 'https://3ae2-2a01-cb1c-4ba-5a00-a4cf-b562-239d-b3c0.eu.ngrok.io',
+  local: 'http://localhost:8000',
+  dev: 'http://3ae2-2a01-cb1c-4ba-5a00-a4cf-b562-239d-b3c0.eu.ngrok.io',
+  devhttps: 'https://3ae2-2a01-cb1c-4ba-5a00-a4cf-b562-239d-b3c0.eu.ngrok.io',
   // MOCK: 'http://localhost:3001',
 };
