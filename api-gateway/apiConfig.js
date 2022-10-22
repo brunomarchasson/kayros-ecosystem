2b0114c5c87e90d8b4adf3ca277516f57d31316c
brunomarchasson <brunomarchasson@users.noreply.github.com>
2022-10-22 12:33:47 +0200

    Update apiConfig.js

--- api-gateway/apiConfig.js
+++ api-gateway/apiConfig.js
@@ -1,11 +1,10 @@
 
 module.exports = {
   provetiq: 'http://93.12.25.82:8850',
+  provetiqhttps: 'https://93.12.25.82:8850',
 
-
-  local: 'http://localhost:8000',
   local: 'http://localhost:8000',
   dev: 'http://3ae2-2a01-cb1c-4ba-5a00-a4cf-b562-239d-b3c0.eu.ngrok.io',
-  devhttps: 'https://3ae2-2a01-cb1c-4ba-5a00-a4cf-b562-239d-b3c0.eu.ngrok.io',
+  devhttps: 'https://f043-2a01-cb1c-4ba-5a00-a4cf-b562-239d-b3c0.eu.ngrok.io',
   // MOCK: 'http://localhost:3001',
 };
