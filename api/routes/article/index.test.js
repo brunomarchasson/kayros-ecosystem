ebd68db407d4f0a36e0f63c67e0a7da41608a9be
bruno marchasson <bruno.marchasson@gmail.com>
2022-11-28 09:33:27 +0100

    fix tests

--- api/routes/article/index.test.js
+++ api/routes/article/index.test.js
@@ -1,25 +1,25 @@
 import mockControler from "../../utils/testUtils/controler";
 import request from "supertest";
 import server from '../../server';
-import paramsController from '../../controller/article.controller';
+import articlesController from '../../controller/article.controller';
 
 jest.mock('../../core/db')
 jest.mock('../../middleware/authJWT');
 jest.mock('../../controller/article.controller', () => mockControler());
 
-describe("params routes", () => {
+describe("artices routes", () => {
   it("shoult respond to GET ALL", async () => {
     const response = await request(server)
     .get(`/api/article`)
     .expect(200);
     expect(response).toHaveProperty("body");
-    expect(paramsController.getAll).toHaveBeenCalled();
+    expect(articlesController.getAll).toHaveBeenCalled();
   });
   it("shoult not respond GET", async () => {
     const response = await request(server)
     .get(`/api/article/12`)
     .expect(200);
     expect(response).toHaveProperty("body");
-    expect(paramsController.get).toHaveBeenCalled();
+    expect(articlesController.get).toHaveBeenCalled();
   });
 });
